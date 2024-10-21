3b95554fc0985b267c5239ef35ff64bd1e6cccb4
Iván Blanco <ivanblanco4m@gmail.com>
2024-10-21 15:39:13 +0200

    tests passed

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,38 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor(questionsValue, timeLimitValue, timeRemainingValue) {
+        this.questions = questionsValue
+        this.timeLimit = timeLimitValue
+        this.timeRemaining = timeRemainingValue
+        this.correctAnswers = 0
+        this.currentQuestionIndex = 0
+    }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex]
+    }
 
-    // 4. shuffleQuestions()
+    moveToNextQuestion() {
+        this.currentQuestionIndex++
+    }
 
-    // 5. checkAnswer(answer)
+    shuffleQuestions() {
+        for (let i = this.questions.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+        }
+    }
 
-    // 6. hasEnded()
+    checkAnswer(answer) {
+        if (answer === this.getQuestion().answer)
+            return this.correctAnswers++
+    }
+
+    hasEnded() {
+        if (this.currentQuestionIndex < this.questions.length) {
+            return false
+        }
+        if (this.currentQuestionIndex = this.questions) {
+            return true
+        }
+    }
 }
