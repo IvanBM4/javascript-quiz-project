02915072b8cbde2c2b69e6c4f2016877d743dc42
Mercedes <mrcdssebastian@gmail.com>
2024-10-24 12:58:24 +0200

    solved lab

--- src/index.js
+++ src/index.js
@@ -45,21 +45,40 @@ document.addEventListener("DOMContentLoaded", () => {
 
   /************  SHOW INITIAL CONTENT  ************/
 
-  // Convert the time remaining in seconds to minutes and seconds, and pad the numbers with zeros if needed
-  const minutes = Math.floor(quiz.timeRemaining / 60).toString().padStart(2, "0");
-  const seconds = (quiz.timeRemaining % 60).toString().padStart(2, "0");
+  const updateTimer = () => {
+    // Convert the time remaining in seconds to minutes and seconds, and pad the numbers with zeros if needed
+    const minutes = Math.floor(quiz.timeRemaining / 60).toString().padStart(2, "0");
+    const seconds = (quiz.timeRemaining % 60).toString().padStart(2, "0");
 
-  // Display the time remaining in the time remaining container
-  const timeRemainingContainer = document.getElementById("timeRemaining");
-  timeRemainingContainer.innerText = `${minutes}:${seconds}`;
+    // Display the time remaining in the time remaining container
+    const timeRemainingContainer = document.getElementById("timeRemaining");
+    timeRemainingContainer.innerText = `${minutes}:${seconds}`;
 
+  }
   // Show first question
   showQuestion();
 
 
   /************  TIMER  ************/
 
-  let timer;
+
+
+  let timer = setInterval(() => {
+    if (quiz.hasEnded()) {
+      clearInterval(timer)
+      return
+    }
+    if (quiz.timeRemaining === 0) {
+
+      clearInterval(timer)
+      showResults()
+      return
+    }
+    quiz.timeRemaining--
+    updateTimer()
+  }, 1000)
+
+
 
 
   /************  EVENT LISTENERS  ************/
@@ -80,6 +99,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // If the quiz has ended, show the results
     if (quiz.hasEnded()) {
       showResults();
+
       return;
     }
 
